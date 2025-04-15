fa930522115d45f29f4581c7f64ccca477d96fc8
farida-azzoug <kessoufa@gmail.com>
2025-04-15 15:35:05 +0200

    page accueil

--- controller/mainController.js
+++ controller/mainController.js
@@ -1,4 +1,13 @@
 export  const mainController ={
     renderHomePage(req,res){
-        res.send("HelloWorld")
-    }};
+        const books = [
+            { img: "book1.jpg", title: "A LA RECHERCHE DE LA LUMIERE", genre: "Biographie", year: "2020" },
+            { img: "book2.jpg", title: "BRISER LES CHAINES", genre: "Roman", year: "2024" },
+            { img: "book3.jpg", title: "TRANSFERENCE", genre: "Thriller", year: "2020" },
+            { img: "book4.jpg", title: "GHIBLI LES ARTISANS DU RÊVE", genre: "Animation", year: "2024" }
+        ];
+        res.render("home", {books});
+    }
+};
+
+
