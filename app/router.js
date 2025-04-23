9a27ecf6badbad0e63acba4cc870588eb7c0ef01
Heba <hebajw0@gmail.com>
2025-04-23 11:02:21 +0200

    route add and delete appuser setup

--- app/router.js
+++ app/router.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { mainController} from "./controllers/mainController.js" // import mainController from "./controller/mainController.js"
  
+import { appuserController } from "./controllers/appuserController.js"; // import appuserController from "./controller/appuserController.js"
 
 export const router = Router();
 // Define the routes
 router.get("/", mainController.renderHomePage);
+
+
+//app user :
+router.get("/users/:id", appuserController.getUserById); // Add the route for fetching user info
+router.post("/user/:id/delete", appuserController.remove); // Add the route for deleting a user
 
